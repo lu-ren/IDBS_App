9a1a1607c3e5031cf9b68dfeedddcb4d2a922689
Luren Wang <lurenwang@gmail.com>
2017-02-17 21:50:10 -0500

    Refreshed page to include video file list

--- app/view/view.py
+++ app/view/view.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
 from app import app
+from glob import glob
+import os
+import json
+
+import pdb
 
 index = Blueprint('index', __name__)
 
 @index.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('index.html')
+    videoPath = app.config['VIDEO_PATH'];
+    videoFiles = glob(os.path.realpath(videoPath) + '/*.avi') 
+    videoFiles.sort()
+    return render_template('index.html', videoFiles=json.dumps(videoFiles))
--- config.py
+++ config.py
@@ -2,6 +2,7 @@ class BasicConfig(object):
     DEBUG = False
     WTF_CSRF_ENABLED = True
     SECRET_KEY = '3jIUW8AMdlVv3gxJg9cRtkFUsyoK1KlK2+Folt77FEY='
+    VIDEO_PATH = '/mnt/UCF101Videos'
 
 class DebugConfig(BasicConfig):
     DEBUG = True
