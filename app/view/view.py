4fd2e6184e24adad1527721bbbf2cd40d3d21fcb
Luren Wang <luren@larsde.cs.columbia.edu>
2017-04-07 19:00:08 -0400

    Got hickle loading done

--- app/view/view.py
+++ app/view/view.py
@@ -1,31 +1,59 @@
-from flask import Blueprint, render_template
 from app import app
+from flask import Blueprint, render_template, session
 from glob import glob
-import os
-import json
+from scipy.misc import imsave
+import base64
 import hickle
-
+import io
+import json
+import numpy
+import os
 import pdb
 
 index = Blueprint('index', __name__)
 
-"""
-Gets the thumbnails and video paths
-"""
-def getVideos():
-    hklFiles = globl(os.path.realpath(hklPath) + '/*000.hkl')
-    hklFiles.sort()
+def getBase64Img(hklFile):
+    a = hickle.load(hklFile)[:, 3, :, :]
+    b = numpy.transpose(a, (1, 2, 0))
+    buf = io.BytesIO()
+    imsave(buf, b, format='png')
+    buf.seek(0)
 
-@index.route('/', methods=['GET', 'POST'])
-def main():
+    return base64.b64encode(buf.read())
+
+def getVideoHklFiles():
     videoPath = app.config['VIDEO_PATH']
     videoFiles = glob(os.path.realpath(videoPath) + '/*.avi') 
     videoFiles.sort()
 
-    thumbnailPath = app.config['THUMBNAIL_PATH']
-    thumbnailFiles = glob(os.path.realpath(thumbnailPath) + '/*.png')
-    thumbnailFiles.sort()
+    hklPath = app.config['HKL_PATH']
+    hklFiles = glob(os.path.realpath(hklPath) + '/*000.hkl')
+    hklFiles.sort()
 
-    videoData = zip(videoFiles, thumbnailFiles)
+    return (videoFiles, hklFiles)
 
-    return render_template('index.html', videoFiles=json.dumps(videoFiles))
+
+@index.route('/loadimage')
+def loadImage():
+    videoFiles, hklFiles = getVideoHklFiles()
+
+    if 'imgLoadCount' not in session:
+        session['imgLoadCount'] = 0
+
+    loaded = session['imgLoadCount']
+    videoData = []
+
+    numLoad = min(len(hklFiles) - loaded, 10)
+
+    for i in range(numLoad):
+        videoData.append((getBase64Img(hklFiles[i]), videoFiles[i]))
+
+    session['imgLoadCount'] += numLoad
+
+    return json.dumps(videoData)
+
+@index.route('/', methods=['GET', 'POST'])
+def main():
+    jsonVideoData = loadImage()
+
+    return render_template('index.html', videoFiles=jsonVideoData)
--- config.py
+++ config.py
@@ -3,7 +3,8 @@ class BasicConfig(object):
     WTF_CSRF_ENABLED = True
     SECRET_KEY = '3jIUW8AMdlVv3gxJg9cRtkFUsyoK1KlK2+Folt77FEY='
     VIDEO_PATH = '/data/UCF/data/UCF101/Videos'
-    THUMBNAIL_PATH = '/home/luren/thumbnails'
+    HKL_PATH = '/data/UCF/preproc/UCF_all_clips'
+    secret_key = 'd*7P==xXSK$8kFM'
 
 class DebugConfig(BasicConfig):
     DEBUG = True
