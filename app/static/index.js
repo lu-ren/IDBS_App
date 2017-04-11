4d96d560b82663b56242ef4e59f94b21960570da
Luren Wang <luren@larsde.cs.columbia.edu>
2017-04-11 15:23:24 -0400

    Mouse drag works now

--- app/static/index.js
+++ app/static/index.js
@@ -1,5 +1,7 @@
 var videoElement;
-var videoNav;
+var videoNavCanvas;
+
+var canvasObj = {}; //Container for video nav canvas properties
 
 //Data is the list [base64encodedData, videoPath]
 function addThumbnails(data) {
@@ -24,13 +26,64 @@ function playVideo(videoPath) {
     videoElement.load();
 }
 
+function mouseDown(e) {
+    let pos = getMousePosition(e);
+
+    videoNavCanvas.style.cursor = 'crosshair';
+    canvasObj.isDrawing = true;
+    canvasObj.startX = pos.x;
+    canvasObj.startY = pos.y;
+}
+
+function mouseMove(e) {
+    if (canvasObj.isDrawing) {
+        let pos = getMousePosition(e);
+        let width = pos.x - canvasObj.startX;
+        let height = pos.y - canvasObj.startY;
+
+        canvasObj.ctx.clearRect(0, 0, videoNavCanvas.width, videoNavCanvas.height);
+        canvasObj.ctx.beginPath();
+        canvasObj.ctx.fillRect(canvasObj.startX, canvasObj.startY,
+            width, height);
+    }
+}
+
+function mouseUp() {
+    canvasObj.isDrawing = false;
+    videoNavCanvas.style.cursor = 'default';
+}
+
+function getMousePosition(e) {
+    var rect = videoNavCanvas.getBoundingClientRect(),
+        scaleX = videoNavCanvas.width / rect.width,
+        scaleY = videoNavCanvas.height / rect.height;
+
+    return {
+        x: (e.clientX - rect.left) * scaleX,
+        y: (e.clientY - rect.top) * scaleY
+    };
+}
+
 function setup() {
     $(document).ready(function () {
-        videoNav = $('#nav-bar')[0];
+        videoNavCanvas = $('canvas')[0];
         videoElement = $('video')[0];
         videoElement.autoplay = true;
 
         addThumbnails(videoData);
+
+        //Setting up the video nav bar
+        canvasObj.ctx = videoNavCanvas.getContext('2d');
+        canvasObj.ctx.fillStyle = '#39acdd';
+        canvasObj.isDrawing = false;
+
+        $('canvas').on('mousedown', function(e) {
+            mouseDown(e);
+        }).on('mouseup', function(e) {
+            mouseUp();
+        }).on('mousemove', function(e) {
+            mouseMove(e);
+        });
     });
 }
 
